64c960d261fba254b17a20697b50c25a91026a24
Lon Sun <lon.sun@gmail.com>
2015-05-26 20:12:41 -0700

    Initial commit. Barebones rails app with postgres, sass, and bootstrap.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RichmondTreesAdmin::Application.config.secret_key_base = '83bffbdc73c68b68f71aeb8ff08b8ca59d8eceefedbdcc2f4e860da90988aa36b572dd05fb1d7b58c70c566148f5bb57b3280a4dcb7783dcdb8160a0fb1dff0c'
