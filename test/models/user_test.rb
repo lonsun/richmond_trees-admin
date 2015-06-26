4efd8f52fcc459e6e5ec868f850cee6f86595f6e
Lon Sun <lon.sun@gmail.com>
2015-06-26 08:54:53 -0700

    Minor formatting fix.

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -5,13 +5,13 @@ class UserTest < ActiveSupport::TestCase
   #   assert true
   # end
   setup do
-  	@user = users(:testuser1);
+    @user = users(:testuser1);
   end
 
   describe "when getting the full name" do
-  	it "gets the full name of user" do
-  		@user.get_full_name.must_equal "test user1"
-  	end
+    it "gets the full name of user" do
+      @user.get_full_name.must_equal "test user1"
+    end
 
     it "omits leading space if first_name is not present" do
       @user.first_name = ""
