5da34883496eb3dd201fa10b7ee7cf8870fed239
Lon Sun <lon.sun@gmail.com>
2015-06-30 16:17:41 -0700

    Use standard active record associations to reference the user that created a note.

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,11 +1,6 @@
 class Note < ActiveRecord::Base
-  belongs_to :user
+  belongs_to :created_by, :class_name => "User", :foreign_key => "user_id"
   belongs_to :planting
 
   validates :planting_id, :user_id, :note, :presence => true
-
-  def created_by_name
-  	user = User.find_by id: self.user_id
-  	user.full_name
-  end
 end
--- test/models/note_test.rb
+++ test/models/note_test.rb
@@ -5,9 +5,4 @@ class NoteTest < ActiveSupport::TestCase
     @note = notes(:one)
   end
 
-  describe "created_by_name method" do
-    it "returns the full name of the user that created the note" do
-      @note.created_by_name.must_equal "test user1"
-    end
-  end
 end
