99f71abcfa8ad3fb0fa2203aa35cbb6ae567ab07
Lon Sun <lon.sun@gmail.com>
2016-02-01 22:14:45 -0800

    Send emails in dev env using mandrill.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -17,7 +17,16 @@ RichmondTreesAdmin::Application.configure do
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.delivery_method = :sendmail
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              ENV['smtp_host'],
+    port:                 587,
+    domain:               "localhost",
+    user_name:            ENV['smtp_username'],
+    password:             ENV['smtp_password'],
+    authentication:       'login',
+    enable_starttls_auto: true
+  }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
