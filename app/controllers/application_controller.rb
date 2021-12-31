83c3cfa23c4a7bbedff5a86e6b2ca9f1bf929e3e
Lon Sun <lon.sun@gmail.com>
2021-12-30 21:47:26 -0800

    maybe defined? does not work the same in ruby 2.6

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,13 +6,10 @@ class ApplicationController < ActionController::Base
   helper :all
   helper_method :current_user_session, :current_user
 
-  
   private
     def current_user_session
-      return @current_user_session if defined?(@current_user_session)
-      current_user_session = UserSession.find
-      logger.info "current_user_session #{current_user_session}"
-      @current_user_session = current_user_session
+      return @current_user_session if @current_user_session
+      @current_user_session = UserSession.find
     end
 
     def current_user
