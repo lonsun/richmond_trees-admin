416caceb40a775dbf561154dfa1445e97bed9017
Lon Sun <lon.sun@gmail.com>
2021-12-30 22:22:53 -0800

    revert, and user current_user_session.user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,16 +8,13 @@ class ApplicationController < ActionController::Base
 
   private
     def current_user_session
-      logger.info "current_user_session: #{@current_user_session}"
-      if defined?(@current_user_session)
-          return @current_user_session
-      end
-      @current_user_session_wut = UserSession.find
+      return @current_user_session if defined?(@current_user_session)
+      @current_user_session = UserSession.find
     end
 
     def current_user
       return @current_user if defined?(@current_user)
-      @current_user = current_user_session && current_user_session.record
+      @current_user = current_user_session && current_user_session.user
     end
 
     def require_user
